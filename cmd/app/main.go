8fb6071d2bb33e348874042c667f735dcea4a375
Nikhod Tursunzoda <tursunzodanikha@gmail.com>
2024-12-08 02:52:20 +0500

    Practice with scheduler in go. New task

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -11,7 +11,7 @@ import (
 )
 
 func main() {
-
+	
 }
 
 func execute() {
--- pkg/solutions/goroutine practices.go
+++ pkg/solutions/goroutine practices.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"math/rand"
 	"os"
+	"runtime"
 	"sync"
 	"time"
 )
@@ -123,6 +124,24 @@ func ChooseOneOption(dataChan chan int, exit chan string) {
 			return
 		}
 	}
+}
+
+func PracticeWithScheduleOfGoroutines() {
+	runtime.GOMAXPROCS(1) // max cores that can be used during the execution of app
+
+	fmt.Printf("FROM MAIN:::		max CPU cores: %d\n", runtime.NumCPU())
+	go CountUntiln(50)
+
+	runtime.Gosched()
+
+	fmt.Println("EXIT")
+}
+
+func CountUntiln(n int) {
+	for i := 0; i < n; i++ {
+		fmt.Printf("FROM GOROUTINE:::	number: %d\n", i)
+	}
+	fmt.Println("THE COUNT IS OVER")
 }
 
 func CorrectWayOfChangingDataWithGoroutines() {
