c1a40e71df16c6f4ac1d8edce31a86e2045af230
Nikhod Tursunzoda <tursunzodanikha@gmail.com>
2024-12-29 12:55:28 +0500

    new file execution for beauty

---
+++ cmd/app/execution.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"Nikcase/internal/Configs"
+	"Nikcase/internal/handlers"
+	"Nikcase/internal/repositories"
+	"Nikcase/internal/services"
+	"Nikcase/pkg/database"
+	"log"
+	"net/http"
+)
+
+func execute() {
+	// Инициализация Конфигов
+	configs, err := Configs.InitConfigs()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	// 	Установка связи с Базой Данных
+	db, err := database.ConnectToDB(configs)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	// Цепочка Работы))
+	repository := repositories.NewRepository(db)
+	service := services.NewService(repository)
+	handler := handlers.NewHandlers(service)
+	log.Println("Main Work is done!")
+
+	// Инициализация Маршрутизатора и передачча поинта в Сервер
+	mux := InitRouters(handler)
+	log.Println("Init MUX is done successfully!")
+
+	srv := http.Server{
+		Addr:    configs.Server.Host + configs.Server.Port,
+		Handler: mux,
+	}
+	err = srv.ListenAndServe()
+	if err != nil {
+		log.Println("Listen and Serv ERROR!!!\n", err)
+		return
+	}
+
+}
