339c61eda956ff87768eee3a9bbfaff43d904850
Nikhod Tursunzoda <tursunzodanikha@gmail.com>
2024-12-29 18:48:46 +0500

    new task: decode several json-objects to slice of struct

--- pkg/practices/other_practices/decode several json-objects.go
+++ pkg/practices/other_practices/decode several json-objects.go
@@ -1,1 +1,46 @@
 package other_practices
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+)
+
+func DecodeSeveralJsonObjects() {
+	var person []persons
+	queue := map[int]string{
+		1: "FIRST",
+		2: "SECOND",
+		3: "THIRD",
+		4: "FOURTH",
+		5: "FIFTH",
+	}
+
+	bytesFromFile, err := os.ReadFile("example.json")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	reader := bytes.NewReader(bytesFromFile)
+	err = json.NewDecoder(reader).Decode(&person)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	for following, value := range person {
+		fmt.Printf("the %s person: %+v\n", queue[following+1], value)
+	}
+}
+
+type persons struct {
+	Id       int
+	Name     string
+	Login    string
+	Password string
+	Active   bool
+	Token    string
+}
