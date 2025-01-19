0c913e90aae3c4dd87f957479b4fa13de3f996a9
Nikhod Tursunzoda <tursunzodanikha@gmail.com>
2025-01-20 00:56:41 +0500

    garbage collector practice and using sync.pool for oprimization

--- pkg/practices/other_practices/garbage_collector.go
+++ pkg/practices/other_practices/garbage_collector.go
@@ -1,1 +1,47 @@
 package other_practices
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"runtime"
+	"sync"
+)
+
+/*print all info about garbage collector property*/
+func GCinfo() {
+	var stats runtime.MemStats
+	err := os.Setenv("GOGC", "50")
+	if err != nil {
+		log.Println(err)
+	}
+
+	runtime.ReadMemStats(&stats)
+	fmt.Printf("garbage collector is set to %s\n", os.Getenv("GOGC"))
+	fmt.Println("HeapAlloc:", stats.HeapAlloc)         // Текущий размер кучи
+	fmt.Println("HeapSys:", stats.HeapSys)             // Общий размер кучи
+	fmt.Println("GCCPUFraction:", stats.GCCPUFraction) // Процент времени, затрачиваемого на GC
+}
+
+// one of the best practice in topic 'garbage collector' is using the pool
+func PoolInGC() {
+	pool := sync.Pool{
+		New: func() any {
+			buf := make([]byte, 1024)
+			return buf
+		},
+	}
+
+	for i := 0; i < 15; i++ {
+		data := pool.Get().([]byte)
+		someAction(data)
+
+		pool.Put(data)
+	}
+}
+
+func someAction(data []byte) {
+	fmt.Println(string(data))
+	fmt.Println(len(data))
+	fmt.Println()
+}
