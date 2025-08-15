3ab627388df357068d3286402ff4fddfd74c4ccb
Nikhod Tursunzoda <tursunzodanikha@gmail.com>
2025-08-15 15:46:24 +0500

    fix

--- pkg/practices/my_context/sharing_value.go
+++ pkg/practices/my_context/sharing_value.go
@@ -1,23 +1,23 @@
-package context
+package my_context
 
 import (
 	"context"
 	"net/http"
 )
 
-const KeyUserId string = "my_user_key"
+const keyUserId string = "my_user_key"
 
 // one can use this func for taking user_id, but don't forget
 // rename the func to naming starting from great register
 func getUserIdByContext(ctx context.Context) (userID int) {
-	return ctx.Value(KeyUserId).(int)
+	return ctx.Value(keyUserId).(int)
 }
 
 // emulation of working middleware
 func UserIdWriter(next http.Handler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		userID := takeUserIdByLoginAndPass("my_login", "my_hashed_pass")
-		ctx := context.WithValue(r.Context(), KeyUserId, userID)
+		ctx := context.WithValue(r.Context(), keyUserId, userID)
 		r = r.WithContext(ctx) // перезапись контекста http запроса - важно
 		next.ServeHTTP(w, r)
 		// after endpoint's code
